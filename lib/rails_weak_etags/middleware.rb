0cb5d89cae2e86d0918c29bd3a8380bba3b97502
John Naegle <john.naegle@goodmeasures.com>
2015-02-26 12:35:15 -0600

    For use with Rack 1.6, restore the request header after converting weak e-tags to strong around invoking the app.

--- lib/rails_weak_etags/middleware.rb
+++ lib/rails_weak_etags/middleware.rb
@@ -6,18 +6,24 @@ module RailsWeakEtags
     end
 
     def call(env)
-      # make request etags "strong"
-      etag = env['HTTP_IF_NONE_MATCH']
-      if etag && etag =~ /^W\//
-        env['HTTP_IF_NONE_MATCH'] = etag[2..-1]
+      status, headers, body = nil, nil, nil
+
+      # make request etags "strong" before calling the app.  Restore the original
+      # header before passing control back.  Rack 1.6 added weak e-tag support
+      # for digest bodies, so it needs to see the weak etag.
+      request_etag = env['HTTP_IF_NONE_MATCH']
+      if request_etag && request_etag =~ /^W\//
+        env['HTTP_IF_NONE_MATCH'] = request_etag[2..-1]
+        status, headers, body = @app.call(env)
+        env['HTTP_IF_NONE_MATCH'] = request_etag
+      else
+        status, headers, body = @app.call(env)
       end
 
-      status, headers, body = @app.call(env)
-
-      # make response etags "weak"
-      etag = headers['ETag']
-      if etag && etag !~ /^W\//
-        headers['ETag'] = "W/#{etag}"
+      # make all response etags "weak"
+      response_etag = headers['ETag']
+      if response_etag && response_etag !~ /^W\//
+        headers['ETag'] = "W/#{response_etag}"
       end
 
       [status, headers, body]
--- lib/rails_weak_etags/railtie.rb
+++ lib/rails_weak_etags/railtie.rb
@@ -1,7 +1,7 @@
 module RailsWeakEtags
   class Railtie < ::Rails::Railtie
     initializer "rails_weak_etags.configure_rails_initialization" do |app|
-      app.middleware.insert_before(Rack::ConditionalGet, RailsWeakEtags::Middleware)
+      app.middleware.insert_before(Rack::ETag, RailsWeakEtags::Middleware)
     end
   end
 end
